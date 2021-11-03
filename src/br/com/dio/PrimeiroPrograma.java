180552ebbead9d593c9a458f79ee9d20d91d488d
Lucas Ito <ito.lucas@gmail.com>
2021-11-03 10:45:51 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,9 +7,48 @@ public class PrimeiroPrograma {
 
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro livro1 = new Livro("O andar do Bebado",300);
+        System.out.println(livro1);
 /*        int a = 1;
         int b = 5;
 
         System.out.println("Hello Word!" + (a+b));*/
+
+
+
     }
 }
+    class Livro{
+        private String nome;
+        private Integer NumPag;
+
+        public Livro(String nome, Integer numPag) {
+            this.nome = nome;
+            NumPag = numPag;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPag() {
+            return NumPag;
+        }
+
+        public void setNumPag(Integer numPag) {
+            NumPag = numPag;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", NumPag=" + NumPag +
+                    '}';
+        }
+    }
